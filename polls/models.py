7ffc997ecb988f7f5132e45d4b4f32debce3c332
Promise30 <promiseadeagbo@gmail.com>
2022-06-30 10:31:46 +0100

    lms: polls: Create a new app 'polls'

---
+++ polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    # id = models.PrimaryKey(auto_increment=True)   Django automatically does the numbering for us
+    question_text = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+class Choices(models.Model):
+    # number of people who took part in the poll
+    # link to a question
+    # choice_of_answer (Yes/No)
+    # votes
+    # id = models.PrimaryKey(auto_increment=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_answer = models.CharField(max_length=20)
+    votes = models.IntegerField(default=0)
